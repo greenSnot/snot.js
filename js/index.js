1f0d9cddd7c5d2986ee0df2c294c12b477d24e11
Patrick <0x00111111@gmail.com>
2016-10-27 22:01:43 +0800

    basic example

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ function onSpriteClick(data){
   console.log(data);
   alert('onSpriteClick');
 }
+
 function on_click(x, y, z, rx, ry) {
   snot.load_sprites([{
     //For CSS Renderer
@@ -41,31 +42,6 @@ var sprites = {
   }
 };
 
-var bullets_pool = [];
-var bullets_running = [];
-var bullet_shooting_range = 400;
-var bullet_pool_size = 100;
-var max_bullets = 35;
-for (var i = 0 ;i < bullet_pool_size; ++i) {
-  var id = 'bullet' + i;
-  sprites[id] = {
-    template:'template-bullet',
-    id:id,
-    x:0,
-    y:-1,
-    z:0,
-
-    dist_x: 0,
-    dist_y: 0,
-    dist_z: bullet_shooting_range,
-    status: -1,
-    visible: false,
-
-    velocity:15, // 1 px per frame
-  };
-  bullets_pool.push(id);
-}
-
 snot.init({
   bg_size:1248,
   bg_imgs:[
@@ -91,69 +67,5 @@ snot.init({
   sprites: sprites
 });
 
-let last_shoot_time = 0;
-let shoot_interval = 80; //ms
-function shoot() {
-  let now = new Date().valueOf();
-  if (now - last_shoot_time > shoot_interval) {
-    bullets_running.push(bullets_pool.pop());
-    last_shoot_time = now;
-  }
-}
-function destory_bullet(id) {
-  for (var i = 0;i < bullets_running.length; ++i) {
-    if (id == bullets_running[i]) {
-      bullets_running.splice(i, 1);
-      bullets_pool.push(id);
-      return;
-    }
-  }
-}
 
-var bullet_offset_y = 50;
-function update() {
-  requestAnimationFrame(update);
-  snot.update();
-  if (bullets_running.length != max_bullets) {
-    shoot();
-  }
-  for (var i in bullets_running) {
-    var id = bullets_running[i];
-    var bullet = snot.sprites[id];
-    if (bullet.status == -1) {
-      bullet.y = - bullet_offset_y;
-      // init or reset
-      bullet.distanceToOrigin = snot.util.distance3D(bullet.dist_x, bullet.dist_y, bullet.dist_z, 0, 0, 0);
-      bullet.steps = Math.floor(bullet.distanceToOrigin / bullet.velocity);
-      bullet.step_x = (bullet.dist_x - bullet.x) * bullet.velocity / bullet.distanceToOrigin;
-      bullet.step_y = (bullet.dist_y - bullet.y) * bullet.velocity / bullet.distanceToOrigin;
-      bullet.step_z = (bullet.dist_z - bullet.z) * bullet.velocity / bullet.distanceToOrigin;
-      bullet.visible = true;
-      bullet.status = 0;
-      bullet.need_update_visibility = true;
-    }
-    if (!bullet.steps) {
-      snot.sprites[id].dist_x = snot.camera_look_at.x * bullet_shooting_range;
-      snot.sprites[id].dist_y = snot.camera_look_at.y * bullet_shooting_range;
-      snot.sprites[id].dist_z = snot.camera_look_at.z * bullet_shooting_range;
-      bullet.x = snot.camera_look_at.x,
-      bullet.y = snot.camera_look_at.y - bullet_offset_y,
-      bullet.z = - snot.camera_look_at.z;
-      bullet.status = -1;
-      bullet.visible = false;
-      bullet.need_update_visibility = true;
-      bullet.need_update_position = true;
-      destory_bullet(id);
-      continue;
-    } else {
-      bullet.x = bullet.x + bullet.step_x;
-      bullet.y = bullet.y + bullet.step_y;
-      bullet.z = bullet.z + bullet.step_z;
-      bullet.steps --;
-    }
-    bullet.need_update_position = true;
-  }
-}
-update();
-
-//snot.run();
+snot.run();
